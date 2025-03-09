fe42309f3e8c1c85f8c34490f363c369f3506bb4
Flórián Lovasi <lovasifloriian@outlook.hu>
2025-03-09 10:45:57 +0100

    create read methods

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  async findAll() {
+    return this.catsService.findAll();
+  }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
+  }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Cat } from './cat';
 import { CatsRepository } from './cats.repository';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Injectable()
 export class CatsService {
   constructor(private readonly catsRepository: CatsRepository) {}
+
+  async findAll() {
+    return this.catsRepository.findAll();
+  }
+
+  async create(dto: CreateCatDto) {
+    const cat = new Cat().setName(dto.name).setAge(dto.age);
+
+    return this.catsRepository.create(cat);
+  }
 }
